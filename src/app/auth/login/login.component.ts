7c886f2da44f859ab9ee99c4758c6893547b9315
Long Nguyen <n.h.long.9697@gmail.com>
2020-08-28 11:57:56 +0700

    change to css

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -9,7 +9,7 @@ import { throwError } from 'rxjs';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss'],
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
   loginForm: FormGroup;
