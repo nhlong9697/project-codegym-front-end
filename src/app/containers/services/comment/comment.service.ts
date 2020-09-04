2e4f970e66fcde3f671a04070702add2c41e1190
tuyendatma <62637890+tuyendatma@users.noreply.github.com>
2020-09-04 10:44:20 +0700

    van loi

--- src/app/containers/services/comment/comment.service.ts
+++ src/app/containers/services/comment/comment.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { CommentPayload } from './../../model/comment/comment.payload';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CommentService {
+  constructor(private httpClient: HttpClient) {}
 
-  constructor() { }
+  getAllCommentsForHouse(houseId: number): Observable<CommentPayload[]> {
+    return this.httpClient.get<CommentPayload[]>(
+      environment.URL + '/api/comments/by-house/' + houseId
+    );
+  }
+
+  postComment(commentPayload: CommentPayload): Observable<any> {
+    return this.httpClient.post<any>(
+      environment.URL + '/api/comments/',
+      commentPayload
+    );
+  }
+
+  getAllCommentsByUser(username: string) {
+    return this.httpClient.get<CommentPayload[]>(
+      environment.URL + '/api/comments/by-house/' +username
+    );
+  }
 }
